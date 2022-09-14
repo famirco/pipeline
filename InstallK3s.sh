96c8a0b9dbda069778082a0b7b42a727ee3a83bd
Amir Dezyanian <30830017+famirco@users.noreply.github.com>
2022-09-14 12:01:29 +0430

    Update InstallK3s.sh

--- InstallK3s.sh
+++ InstallK3s.sh
@@ -6,6 +6,3 @@ else
 #	curl -sfL https://get.k3s.io | sh -
         echo "Install k3s completly install."
 fi
-
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo dpkg -i minikube_latest_amd64.deb
